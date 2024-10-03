537b36d3de73248fd1f91cb63337e7bcbe524bdc
leye195 <leye195@naver.com>
2024-10-04 01:34:30 +0900

    feat: set userAgent before parsing

--- src/hana.ts
+++ src/hana.ts
@@ -14,6 +14,9 @@ export default class HanaBank {
         headless: false,
       });
       const page = await browser.newPage();
+      await page.setUserAgent(
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"
+      );
       await page.goto(this.url, {
         waitUntil: "networkidle2",
       });
